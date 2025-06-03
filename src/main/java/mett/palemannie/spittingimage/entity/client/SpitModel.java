8499ab5650615c5a9f0b37f71a6e85a1525ae7fb
PaleMannie <einalex44@gmail.com>
2025-06-03 23:06:10 +0200

    version 3 almost ported to 1.20.1

---
+++ src/main/java/mett/palemannie/spittingimage/entity/client/SpitModel.java
@@ -0,0 +1,41 @@
+package mett.palemannie.spittingimage.entity.client;
+
+import mett.palemannie.spittingimage.SpittingImage;
+import mett.palemannie.spittingimage.entity.custom.SpitEntity;
+import net.minecraft.client.model.*;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.render.entity.model.EntityModelLayer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+
+public class SpitModel extends EntityModel<SpitEntity> {
+
+    public static final EntityModelLayer SPIT = new EntityModelLayer(Identifier.of(SpittingImage.MODID, "spit"), "main");
+    private final ModelPart spit;
+    public SpitModel(ModelPart root) { this.spit = root.getChild("spit"); }
+
+    public static TexturedModelData getTexturedModelData() {
+
+        ModelData modelData = new ModelData();
+        ModelPartData modelPartData = modelData.getRoot();
+
+        ModelPartData main = modelPartData.addChild("spit", ModelPartBuilder.create()
+                        .uv(3, 3).cuboid(0.0F, 0.0F, -1.0F, 1.0F, 1.0F, 1.0F, new Dilation(0.0F))
+                        .uv(3, 1).cuboid(-1.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F, new Dilation(0.0F))
+                        .uv(0, 2).cuboid(-1.0F, -1.0F, -1.0F, 1.0F, 1.0F, 1.0F, new Dilation(0.0F))
+                        .uv(0, 0).cuboid(0.0F, -1.0F, 0.0F, 1.0F, 1.0F, 1.0F, new Dilation(0.0F))
+                , ModelTransform.NONE);
+
+        return TexturedModelData.of(modelData, 16, 16);
+    }
+
+    @Override
+    public void setAngles(SpitEntity entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) { }
+
+    @Override
+    public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, float red, float green, float blue, float alpha) {
+        spit.render(matrices, vertices, light, overlay, red, green, blue, alpha);
+    }
+}
---
+++ src/main/java/mett/palemannie/spittingimage/entity/client/SpitRenderer.java
@@ -0,0 +1,48 @@
+package mett.palemannie.spittingimage.entity.client;
+
+import mett.palemannie.spittingimage.SpittingImage;
+import mett.palemannie.spittingimage.entity.custom.SpitEntity;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RotationAxis;
+
+@Environment(EnvType.CLIENT)
+public class SpitRenderer extends EntityRenderer<SpitEntity> {
+
+    public static final Identifier TEXTURE = Identifier.of(SpittingImage.MODID, "textures/entity/spit/spit.png");
+    protected SpitModel model;
+
+    public SpitRenderer(EntityRendererFactory.Context context) {
+        super(context);
+        this.model = new SpitModel(context.getPart(SpitModel.SPIT));
+    }
+
+    public void render(SpitEntity spitEntity, float yaw, float tickDelta, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int light) {
+
+        matrixStack.push();
+
+        matrixStack.translate(0.0F, 0.1f, 0.0F);
+
+        matrixStack.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(MathHelper.lerp(tickDelta, spitEntity.prevYaw, spitEntity.getYaw()) + 90.0F));
+        matrixStack.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(MathHelper.lerp(tickDelta, spitEntity.prevPitch, spitEntity.getPitch())));
+
+        VertexConsumer vertexConsumer = vertexConsumerProvider.getBuffer(RenderLayer.getEntityCutout(TEXTURE));
+        this.model.render(matrixStack, vertexConsumer, light, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
+        matrixStack.pop();
+
+        super.render(spitEntity, yaw, tickDelta, matrixStack, vertexConsumerProvider, light);
+    }
+
+    public Identifier getTexture(SpitEntity SpitEntity) {
+        return TEXTURE;
+    }
+}
